f9a166026705e6cae358a514febaf86954a839ed
Andrew Huffman <ahuffman@idc.com>
2015-06-01 10:01:14 -0400

    Fixing repeated node changes on yumcache by making this a refreshonly

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -143,9 +143,10 @@ define packagecloud::repo(
         }
 
         exec { "yum_make_cache_${repo_name}":
-          command => "yum -q makecache -y --disablerepo='*' --enablerepo='${normalized_name}'",
-          path => "/usr/bin",
-          require => File["${normalized_name}"],
+          refreshonly => true,
+          command     => "yum -q makecache -y --disablerepo='*' --enablerepo='${normalized_name}'",
+          path        => "/usr/bin",
+          require     => File["${normalized_name}"],
         }
       }
 
